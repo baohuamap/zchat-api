4717334b27e7b2f67a930987d06b6291418aa5f2
bhmap <baohua3110@gmail.com>
2025-03-20 21:43:09 +0700

    add init user model & repo

--- repositories/user.go
+++ repositories/user.go
@@ -1,1 +1,23 @@
 package repositories
+
+import (
+	"github.com/baohuamap/zchat-api/models"
+
+	"gorm.io/gorm"
+)
+
+type UserRepository interface {
+	Create(user models.User) error
+}
+
+type user struct {
+	DB *gorm.DB
+}
+
+func ProvideUserRepository(DB *gorm.DB) UserRepository {
+	return &user{DB: DB}
+}
+
+func (r user) Create(user models.User) error {
+	return r.DB.Create(&user).Error
+}
